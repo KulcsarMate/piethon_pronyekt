05b261044d59ab5ca53ca1946d3bf2e0b1b0a1c2
kulcsar.mate <kulcsar.mate@ckik.hu>
2024-04-15 16:58:04 +0200

    Tesztelés és javítás

--- IDK_yet.py
+++ IDK_yet.py
@@ -1,13 +1,22 @@
 def válassz(lista):
     while True:
-        elem = input("Adja meg a keresni kívánt programozói tétel nevét: ")
+        elem = input("Adja meg a keresni kívánt programozói tétel nevét: ").capitalize()
         good = search(lista, elem)
         if good == False:
             print("Adjon meg egy választ a lehetőségek közül")
         else:
             return good
 
-def alkalmaz(szám = int):
+def search(lista, elem):
+    i = 0
+    while i < len(lista)-1 and not(elem == lista[i]):
+        i += 1
+    if not(i < len(lista)-1):
+        return False
+    else:
+        return i
+
+def alkalmaz(szám):
     if szám == 0:
         megszámolás()
     elif szám == 1:
@@ -24,38 +33,33 @@ def alkalmaz(szám = int):
         rendezés()
 
 def megszámolás():
-    ...
+    print("asd")
 
 def összegzés():
-    ...
+    print("asd")
 
 def minimum():
-    ...
+    print("asd")
 
 def maximum():
-    ...
+    print("asd")
 
 def keresés():
-    ...
+    print("asd")
 
 def kiválogatás():
-    ...
+    print("asd")
 
 def rendezés():
-    ...
+    print("asd")
 
-def search(lista, elem):
-    i = 0
-    while i < len(lista) or not(elem == lista[i]):
-        i += 1
-    if not(i < len(lista)):
-        return False
-    else:
-        return i
+def niceprint(lista):
+    for i in range(len(lista)-1):
+        print(lista[i], end="; ")
+    print(lista[len(lista)-1])
 
 def main():
-    lista = []
-    alkalmaz(válassz(lista))
-
+    lista = ["Megszámolás", "Összegzés", "Minimum", "Maximum", "Keresés", "Kiválogatás", "Rendezés"]
+    alkalmaz(int(válassz(lista)))
 
 main()
